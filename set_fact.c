1f3694998c0b5e54c7da0bcbeb466974966c4dff
1seanob1337 <73853862+1seanob1337@users.noreply.github.com>
2023-08-21 20:10:34 -0400

    Create set_fact.c

---
+++ set_fact.c
@@ -0,0 +1,29 @@
+* new_fact.c – Computes the factorial of an integer entered
+* by the user. Program terminates when the integer entered is not
+* positive.
+*/
+#include <stdio.h>
+unsigned long long fact(int n);
+int main(void) {
+int current;
+// change to zero since unsigned
+printf("Enter a positive integer [to terminate enter zero] > ");
+scanf("%d", &current);
+while (current > 0) {
+printf("The factorial of %d is %llu\n", current, fact(current));
+printf("Enter a positive integer [to terminate enter non-positive] > ");
+scanf("%d", &current);
+}
+return 0;
+}
+/* n is a positive integer. The function returns n factorial */
+unsigned long long fact(int n) {
+int lcv; /* loop control variable */
+unsigned long long p; /* set to the product of the first lcv positive integers */
+for (p=1, lcv=2; lcv <= n; lcv++)
+{
+p *= lcv;
+// p = p*lcv;
+}
+return p;
+};
